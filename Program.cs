53bd05efbe814111f04262c80995d8a2a16e57a8
Dmitriy Malikov <dmalikov@rdvb.ru>
2018-12-25 16:52:17 +0500

    Palindrome https://www.testdome.com/questions/c-sharp/palindrome/7282

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        var rev = Reverse(word);
+        return String.Equals(word.ToLower(), rev.ToLower());
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
